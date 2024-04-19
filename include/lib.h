6c72693a94096ff1ec780637d5b6cfca198ec4a3
IwM341 <artem_tovstun@mail.ru>
2024-04-19 17:55:53 +0300

    all

---
+++ include/lib.h
@@ -0,0 +1,6 @@
+#ifndef LIB_H
+#define LIB_H
+
+int lib_function(int a, int b);
+
+#endif
---
+++ src/lib.cpp
@@ -0,0 +1,5 @@
+#include <lib.h>
+#include <grob/grid.hpp>
+int lib_function(int a, int b){
+    return a*b;
+}
